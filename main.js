f7231bad372133993cc0dfcbcfeb89c0724dc599
NafisaHassan114 <nafisanafta469@gmail.com>
2024-03-11 16:58:00 +0300

    these are operations in js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let n = parseFloat(prompt("Enter the first number"));
+let c = parseFloat(prompt("Enter the second number"));
+
+console.log(n + c)
+console.log(n - c)
+console.log(n * c)
+console.log(n / c)
+console.log(n % c)
+
+
+
+
+
